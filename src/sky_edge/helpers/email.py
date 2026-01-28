b09a427b7822399a7bb624a973d6518d19e0fef7
Zachary Curtis <zjcurtis@proton.me>
2026-01-28 12:28:24 -0500

    added a duplicate email helper.

---
+++ src/sky_edge/helpers/email.py
@@ -0,0 +1,24 @@
+from sky_edge.api.constituent import (
+    CollectionOfEmails,
+    Email,
+    email_list_constituent_get,
+)
+
+
+def get_duplicate_emails(
+    constituent_id: str, include_inactive: bool = False
+) -> dict[str, Email] | None:
+    email_list_response = email_list_constituent_get(constituent_id)
+    if isinstance(email_list_constituent_get, CollectionOfEmails):
+        duplicate_dictionary = {}
+        for email in email_list_response.value:
+            if email.address in duplicate_dictionary:
+                duplicate_dictionary[email.address].append(email)
+            else:
+                duplicate_dictionary[email.address] = [email]
+
+        for k, v in duplicate_dictionary.items():
+            if len(v) == 1:
+                duplicate_dictionary.pop(k)
+
+        return duplicate_dictionary
