3c9cde2c3a59a03b3b45e85aa6536b475f025c94
Zachary Curtis <zjcurtis@proton.me>
2026-01-28 12:46:23 -0500

    fixed type annotation.

--- src/sky_edge/helpers/email.py
+++ src/sky_edge/helpers/email.py
@@ -7,7 +7,7 @@ from sky_edge.api.constituent import (
 
 def get_duplicate_emails(
     constituent_id: str, include_inactive: bool = False
-) -> dict[str, Email] | None:
+) -> dict[str, list[Email]] | None:
     email_list_response = email_list_constituent_get(constituent_id)
     if isinstance(email_list_response, CollectionOfEmails):
         duplicate_dictionary = {}
